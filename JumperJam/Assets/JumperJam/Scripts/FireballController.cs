a42bb767549eaf90a91d35c23020d63ab3a712a1
H2V <h2v9696@gmail.com>
2017-05-12 10:00:30 +0700

    Add some Enemies
    Add more enemies

---
+++ JumperJam/Assets/JumperJam/Scripts/FireballController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballController : MonoBehaviour 
+{
+	[SerializeField]
+	private float duration;
+
+	public void Shoot (float speed) {
+		GetComponent<Rigidbody2D> ().velocity = new Vector2(speed, GetComponent<Rigidbody2D> ().velocity.y);
+	}
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			Destroy ();
+		} 
+	}
+
+	public void Destroy() {
+		ContentMgr.Instance.Despaw (gameObject);
+	}
+}
