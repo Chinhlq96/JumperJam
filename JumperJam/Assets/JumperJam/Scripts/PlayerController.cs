00f313c0cecb8b0302a7b2f82a9653a4ddd893f2
H2V <h2v9696@gmail.com>
2017-06-28 17:18:06 +0700

    nothing

--- JumperJam/Assets/JumperJam/Scripts/PlayerController.cs
+++ JumperJam/Assets/JumperJam/Scripts/PlayerController.cs
@@ -7,40 +7,38 @@ using DG.Tweening;
 public class PlayerController : SingletonMonoBehaviour<PlayerController>
 {
 
-    /// <summary>
-    /// force add to jump
-    /// </summary>
-    [SerializeField]
-    Vector2 force;
-
-    /// <summary>
-    /// sprite renderer of player
-    /// </summary>
-    [SerializeField]
-    SpriteRenderer playerSR;
-
-    /// <summary>
-    /// RigidBody
-    /// </summary>
-    [SerializeField]
-    Rigidbody2D RG;
-
-    /// <summary>
-    /// Transform of player
-    /// </summary>
-    [SerializeField]
-    Transform playerTrans;
-
+	/// <summary>
+	/// force add to jump
+	/// </summary>
 	[SerializeField]
-	private Transform[] backGround;
+	Vector2 force;
 
-    /// <summary>
-    /// 1 die 
-    /// 2 idle
-    /// 3 jump
-    /// </summary>
-    
-    public Sprite[] aniSprites;
+	/// <summary>
+	/// sprite renderer of player
+	/// </summary>
+	[SerializeField]
+	SpriteRenderer playerSR;
+
+	/// <summary>
+	/// RigidBody
+	/// </summary>
+	[SerializeField]
+	Rigidbody2D RG;
+
+	/// <summary>
+	/// Transform of player
+	/// </summary>
+	[SerializeField]
+	Transform playerTrans;
+
+
+	/// <summary>
+	/// 1 die 
+	/// 2 idle
+	/// 3 jump
+	/// </summary>
+
+	public Sprite[] aniSprites;
 
 
 	public Transform pos;
@@ -51,7 +49,7 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
 	private bool notTouchOne;
 	//
 
-    private PlayerState _playerState;
+	private PlayerState _playerState;
 
 	//player can move horizontal or not
 	public bool canMoveNow;
@@ -60,43 +58,43 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
 	private bool groundTouched;
 	public bool groundDeath;
 
-	//Camera follow 
-	public GameObject camera;
 
-    public PlayerState playerState
-    {
-        get { return _playerState; }
-        set
-        {
-            _playerState = value;
-            UpdateState();
-        }
-    }
+
+	public PlayerState playerState
+	{
+		get { return _playerState; }
+		set
+		{
+			_playerState = value;
+			UpdateState();
+		}
+	}
 
 
 
 	private Vector3 startPos;
 	public Vector3 maxPos;
 
-    void OnEnable()
-    {
+	void OnEnable()
+	{
+		transform.GetChild(0).GetComponent<Collider2D> ().enabled = true;
 		startPos = transform.position;
 		maxPos = startPos;
 		DOTween.Init();
-        playerState = PlayerState.Idle;
+		playerState = PlayerState.Idle;
 		canMoveNow = false;
 		notTouchOne = true;
-    }
+	}
 
-    /// <summary>
-    /// Update state while change state
-    /// </summary>
-    public void UpdateState()
-    {
-        int index = (int)playerState;
-        playerSR.sprite = aniSprites[index];
-        playerTrans.localRotation = Quaternion.Euler(new Vector3(0, 0, playerState == PlayerState.Die ? -30 : 0));
-    }
+	/// <summary>
+	/// Update state while change state
+	/// </summary>
+	public void UpdateState()
+	{
+		int index = (int)playerState;
+		playerSR.sprite = aniSprites[index];
+		playerTrans.localRotation = Quaternion.Euler(new Vector3(0, 0, playerState == PlayerState.Die ? -30 : 0));
+	}
 
 	public void resetPosition()
 	{
@@ -122,12 +120,12 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
 			transform.position = pos;
 
 
-				///
+			///
 
-				moveX = Input.GetAxisRaw ("Horizontal");
-				Vector2 directionX = new Vector2 (moveX, 0);
-				MoveX (directionX);
-		
+			moveX = Input.GetAxisRaw ("Horizontal");
+			Vector2 directionX = new Vector2 (moveX, 0);
+			MoveX (directionX);
+
 
 		}
 
@@ -177,71 +175,80 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
 		transform.position = pos;
 	}
 
-    public void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.CompareTag("Platform"))
-        {
-			Jump ();
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.CompareTag("Platform")||col.CompareTag("SPlatform"))
+		{
+			if (col.CompareTag ("SPlatform")) {
+				Jump (new Vector2 (0, 80f));
+
+			} else
+				Jump (force);
 
 			//make platform bounce
 
 
 
-			if (notTouchOne == true) 
-			{
-				Jump ();
-				transform.eulerAngles = new Vector3 (0,0,0);
-				if (moveX > 0 || Input.acceleration.x > 0) {
-					transform.DORotate (new Vector3 (0, 0, -360), 0.5f, RotateMode.FastBeyond360);
-				} else if (moveX <= 0 || Input.acceleration.x < 0){
-					transform.DORotate (new Vector3 (0, 0, +360), 0.5f, RotateMode.FastBeyond360);
-				}
-				notTouchOne = false;
-			}
-        }
+			//			if (notTouchOne == true) 
+			//			{
+			//				Jump (force);
+			//				transform.eulerAngles = new Vector3 (0,0,0);
+			//				if (moveX > 0.5f || Input.acceleration.x > 0.5f) {
+			//					transform.DORotate (new Vector3 (0, 0, -360), 0.5f, RotateMode.FastBeyond360);
+			//				} else if (moveX < 0.5f || Input.acceleration.x < 0.5f){
+			//					transform.DORotate (new Vector3 (0, 0, +360), 0.5f, RotateMode.FastBeyond360);
+			//				}
+			//				notTouchOne = false;
+			//			}
+		}
 		if (col.CompareTag("Enemy")||col.CompareTag("Bullet"))
-        {
-
-			if ((transform.position.y > col.transform.position.y + 1f)&&!col.CompareTag("Bullet"))
-			{
+		{
+			if ((transform.position.y > col.transform.position.y + 0.7f)&&!col.CompareTag("Bullet")) {
 				RG.velocity = new Vector2(0, 0);
 				RG.AddForce(force * 1f, ForceMode2D.Impulse);
 				col.gameObject.SetActive (false);
 				ScoreMgr.Instance.AddScore (col.gameObject.GetComponent<EnemyPatrol> ().point);
-			} 
-			else 
-			{
+			} else {
+				playerState = PlayerState.Die;
 				Die ();
+				ScoreMgr.Instance.UpdateGameOverScore ();
+				MapMgr.Instance.resetDifficult ();
+				GameMgr.Instance.GameOver();
 			}
 
-        }
+		}
 
 		if (col.CompareTag ("DeathArea")) 
 		{
-
+			playerState = PlayerState.Die;
 			Die();
-
+			ScoreMgr.Instance.UpdateGameOverScore ();
+			//reset difficult
+			MapMgr.Instance.resetDifficult ();
+			GameMgr.Instance.GameOver();
 		}
 
 		if (col.CompareTag ("Ground")) 
 		{
-			if (groundTouched) 
-			{
-
+			if (groundTouched) {
+				playerState = PlayerState.Die;
 				Die ();
+				ScoreMgr.Instance.UpdateGameOverScore ();
 
 				groundDeath = true;
-			
 
+
+				//reset difficult
+				MapMgr.Instance.resetDifficult ();
+				GameMgr.Instance.GameOver ();
 			}
 		}
-       
-    }
+
+	}
 
 	public void OnTriggerExit2D(Collider2D col)
 	{
-		if (col.CompareTag ("Ground")) 
-		{
+		if (col.CompareTag ("Ground")) {
 			groundTouched = true;
 		}
 	}
@@ -249,69 +256,62 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
 	IEnumerator Bound(Collider2D col)
 	{
 		if (transform.position.y > col.transform.position.y) 
-						{
-							col.transform.DOLocalMoveY (col.transform.localPosition.y - 0.3f, 0.1f).OnComplete (() => {
-								col.transform.DOLocalMoveY (col.transform.localPosition.y + 0.3f, 0.1f);
-							});
-						}
+		{
+			col.transform.DOLocalMoveY (col.transform.localPosition.y - 0.3f, 0.1f).OnComplete (() => {
+				col.transform.DOLocalMoveY (col.transform.localPosition.y + 0.3f, 0.1f);
+			});
+		}
 		yield return new WaitForSeconds (0.1f);
 	}
 
-    public void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.CompareTag("Ground") && playerState == PlayerState.Die)
-        {
-            GameMgr.Instance.GameOver();
-        }
-    }
+	public void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.CompareTag("Ground") && playerState == PlayerState.Die)
+		{
+			GameMgr.Instance.GameOver();
+		}
+	}
 
-    public void Jump()
-    {
-	//	Debug.Log (RG.velocity.y);
-        if (RG.velocity.y <= 0)
-        {
-            if (playerState == PlayerState.Die) return;
-            RG.velocity = new Vector2(0, 0);
-            RG.AddForce(force, ForceMode2D.Impulse);
-            playerState = PlayerState.Jump;
-        }
+	public void Jump(Vector2 force)
+	{
+		//	Debug.Log (RG.velocity.y);
+		if (RG.velocity.y <= 0)
+		{
+			if (playerState == PlayerState.Die) return;
+			RG.velocity = new Vector2(0, 0);
+			RG.AddForce(force, ForceMode2D.Impulse);
+			playerState = PlayerState.Jump;
+		}
 		notTouchOne = true;
-    }
+	}
 
-    void Die()
-    {
-        RG.velocity = new Vector2(0, -5);
+	void Die()
+	{
+		transform.GetChild(0).GetComponent<Collider2D> ().enabled = false;
+		RG.velocity = new Vector2(0, -5);
 		canMoveNow = false;
-		playerState = PlayerState.Die;
-		ScoreMgr.Instance.UpdateGameOverScore ();
-		MapMgr.Instance.resetDifficult ();
-		GameMgr.Instance.GameOver();
 
-			backGround [0].GetComponent<CamShake> ().MinorShake (100);
-		backGround [1].GetComponent<CamShake> ().MinorShake (40);
-		backGround [2].GetComponent<CamShake> ().MinorShake (40);
-//		camera.GetComponent<CameraControl> ().followOnDeath ();
-    }
+	}
 
-//void FixedUpdate()	
-//
-//    {
-//		Debug.Log ("run");
-//		if (canMoveNow == true && GameMgr.Instance.gameState != GameState.Pause) {
-//			float moveHorizontal = Input.GetAxis ("Horizontal");
-//			Vector3 movement = new Vector3 (moveHorizontal, 0, 0);
-//			transform.position += movement * 20f * Time.deltaTime;
-//
-//
-//			if (RG.velocity.y < 0 && playerState != PlayerState.Die) {
-//				playerState = PlayerState.Idle;
-//			}
-//		}
-//    }
+	//void FixedUpdate()	
+	//
+	//    {
+	//		Debug.Log ("run");
+	//		if (canMoveNow == true && GameMgr.Instance.gameState != GameState.Pause) {
+	//			float moveHorizontal = Input.GetAxis ("Horizontal");
+	//			Vector3 movement = new Vector3 (moveHorizontal, 0, 0);
+	//			transform.position += movement * 20f * Time.deltaTime;
+	//
+	//
+	//			if (RG.velocity.y < 0 && playerState != PlayerState.Die) {
+	//				playerState = PlayerState.Idle;
+	//			}
+	//		}
+	//    }
 
 }
 
 public enum PlayerState
 {
-    Die = 0, Idle, Jump
+	Die = 0, Idle, Jump
 }
